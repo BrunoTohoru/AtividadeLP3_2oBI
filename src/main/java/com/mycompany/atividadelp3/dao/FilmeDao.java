e46b67009eb1f5f038468e7a3e79fd0eed782b5c
Your Name <you@example.com>
2024-09-27 21:52:08 -0300

    Implementação dos daos 2.2/4

--- src/main/java/com/mycompany/atividadelp3/dao/FilmeDao.java
+++ src/main/java/com/mycompany/atividadelp3/dao/FilmeDao.java
@@ -4,10 +4,69 @@
  */
 package com.mycompany.atividadelp3.dao;
 
+import com.mycompany.atividadelp3.bean.Filme;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author Aluno
  */
-public class FilmeDao {
+public class FilmeDao implements Dao<Integer, Filme>{
+    protected Connection con;
+
+    public FilmeDao(Connection con) {
+        this.con = con;
+    }
+
+    @Override
+    public void create(Filme entity) {
+        String sql = "INSERT INTO filme(nome, ano, duracao, foto, sinopse, estilo_id) VALUES (?, ?, ?, ?, ?, ?)";
+        
+        try {
+            PreparedStatement query = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            query.setString(1, entity.getNome());
+            query.setString(2, entity.getAno());
+            query.setInt(3, entity.getDuracao());
+            query.setString(4, entity.getFoto());
+            query.setString(5, entity.getSinopse());
+            query.setInt(3, entity.getEstilo().getId());
+            query.executeUpdate();
+            
+            ResultSet rs = query.getGeneratedKeys();
+            
+            if(rs.next()){
+                int id = rs.getInt("id");
+                entity.setId(id);
+            }
+            query.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public Filme retrieve(Integer pk) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Filme entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Integer pk) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Filme> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
--- src/main/java/com/mycompany/atividadelp3/dao/LocacaoDao.java
+++ src/main/java/com/mycompany/atividadelp3/dao/LocacaoDao.java
@@ -4,10 +4,44 @@
  */
 package com.mycompany.atividadelp3.dao;
 
+import com.mycompany.atividadelp3.bean.Locacao;
+import java.sql.Connection;
+import java.util.List;
+
 /**
  *
  * @author Aluno
  */
-public class LocacaoDao {
+public class LocacaoDao implements Dao<Integer, Locacao>{
+    protected Connection con;
+
+    public LocacaoDao(Connection con) {
+        this.con = con;
+    }
+
+    @Override
+    public void create(Locacao entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Locacao retrieve(Integer pk) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Locacao entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Integer pk) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Locacao> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
